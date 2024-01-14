8f9ba7185267e5f20e06fd858cf703e14e0fa462
ylahssini <ylahssini@gmail.com>
2024-01-14 23:19:12 +0100

    🔧 support absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import path from 'path';
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
-    solidPlugin(),
-  ],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+    plugins: [
+        /* 
+        Uncomment the following line to enable solid-devtools.
+        For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
+        */
+        // devtools(),
+        solidPlugin(),
+    ],
+    server: {
+        port: 3000,
+    },
+    resolve: {
+        alias: [
+            { find: '@', replacement: path.resolve(__dirname, 'src') },
+        ],
+    },
+    build: {
+        target: 'esnext',
+    },
 });
