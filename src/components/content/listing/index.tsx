34c3eca77143582714e2c06eec1332e2c07cd154
ylahssini <ylahssini@gmail.com>
2024-01-22 00:07:29 +0100

    🔨 add notification to click on emoji

--- src/components/content/listing/index.tsx
+++ src/components/content/listing/index.tsx
@@ -1,6 +1,7 @@
+import { For } from 'solid-js';
+import toast, { Toaster } from 'solid-toast';
 import { useEmojiContext } from '@/context';
 import './styles.css';
-import { For } from 'solid-js';
 
 const colors = ['rose', 'red', 'purple', 'lime', 'orange', 'sky', 'blue', 'amber', 'fuchsia', 'violet', 'green', 'yellow', 'emerald', 'teal', 'cyan', 'indigo'];
 
@@ -11,38 +12,58 @@ function random(min: number, max: number): number {
 const Listing = () => {
     const [state] = useEmojiContext();
 
-    async function handleCopy(emoji: string) {
+    async function handleCopy(emoji: string, color: string) {
         try {
             await navigator.clipboard.writeText(emoji);
-            console.log('Content copied to clipboard');
+            toast.custom(
+                (t) => (
+                    <section class={`toast bg-${color}-100 ${!t.visible ? '__closed' : ''}`}>
+                        <h5 class="font-emoji text-4xl">{emoji}</h5>
+                        <div>
+                            <strong class="text-base">Hey</strong>
+                            <p class="text-sm pb-2">The emoji is copied to clipboard</p>
+                        </div>
+                        <button onClick={() => toast.dismiss(t.id)} type="button" class="close_toast">❌</button>
+                        <span class={`progress bg-${color}-200`} />
+                    </section>
+                ),
+                {
+                    duration: 5000,
+                    position: 'top-right',
+                    unmountDelay: 1000,
+                }
+            );
         } catch (err) {
             console.error('Failed to copy: ', err);
         }
     }
 
     return (
-        <ul class="grid grid-cols-5 grid-rows-5 gap-8 pb-8">
-            <For each={state.emojis.filter((emoji) => state.category !== '' ? (state.category === emoji.category) : true)}>
-                {(emoji) => {
-                    const colorIndex = random(0, colors.length - 1);
-                    const bg = `bg-${colors[colorIndex]}-100 hover:bg-${colors[colorIndex]}-200`;
+        <>
+            <ul class="grid grid-cols-5 grid-rows-5 gap-8 pb-8">
+                <For each={state.emojis.filter((emoji) => state.category !== '' ? (state.category === emoji.category) : true)}>
+                    {(emoji) => {
+                        const colorIndex = random(0, colors.length - 1);
+                        const bg = `bg-${colors[colorIndex]}-100 hover:bg-${colors[colorIndex]}-200`;
 
-                    return (
-                        <li class={`${bg} card`}>
-                            <button
-                                class={`${state.system_font ? '' : 'font-emoji'} cursor-copy text-8xl pb-4`}
-                                type="button"
-                                onClick={() => handleCopy(emoji.emoji)}
-                            >
-                                {emoji.emoji}
-                            </button>
-                            <h4>{emoji.description}</h4>
-                            <small class="text-gray-500">{emoji.category}</small>
-                        </li>
-                    )
-                }}
-            </For>
-        </ul>
+                        return (
+                            <li class={`${bg} card`}>
+                                <button
+                                    class={`${state.system_font ? '' : 'font-emoji'} cursor-copy text-8xl pb-4 transition-transform ease-in-out active:rotate-45 active:scale-125`}
+                                    type="button"
+                                    onClick={() => handleCopy(emoji.emoji, colors[colorIndex])}
+                                >
+                                    {emoji.emoji}
+                                </button>
+                                <h4>{emoji.description}</h4>
+                                <small class="text-gray-500">{emoji.category}</small>
+                            </li>
+                        )
+                    }}
+                </For>
+            </ul>
+            <Toaster />
+        </>
     )
 }
 
