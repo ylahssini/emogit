f162b99477520f26793a76290f1d8cfb38a83577
ylahssini <ylahssini@gmail.com>
2024-02-03 14:50:14 +0100

    ⭐ add footer component

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,17 @@
+import './styles.css';
+
+const Footer = () => {
+    return (
+        <footer class="max-w-[1600px] mt-10 mx-auto py-8 text-center text-sm font-light block sm:flex sm:justify-between">
+            <p>
+                &copy; All rights reserved. Created by <a href="https://ylahssini.vercel.app" class="link">Youssef Lahssini</a>
+            </p>
+
+            <p>
+                Code source in <a href="https://github.com/ylahssini/emogit" class="link">Github</a>
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
